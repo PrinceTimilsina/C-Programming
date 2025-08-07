719682d68dc5a524487ba043f44683f1032560ec
PrinceTimilsina <timilsinaprince07@gmail.com>
2025-08-07 07:48:03 +0545

    C-Programming Revision

---
+++ Example-Program/subtract.cpp
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+int a,b,sub;
+printf("Enter any two numbers:");
+scanf("%d %d",&a,&b);
+sub = a-b;
+printf("The Subtracted result of given number is %d",sub);
+return 0;
+}  
