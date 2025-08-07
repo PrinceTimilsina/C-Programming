0cb4294a5aef7e98e4700d317a55c613e56f3770
PrinceTimilsina <timilsinaprince07@gmail.com>
2025-08-07 07:51:02 +0545

    C-Programming

---
+++ Example-Program/product.cpp
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+	int a,b,product;
+	printf("Enter any two numbers to find their product:");
+	scanf("%d %d",&a,&b);
+	product= a*b;
+	printf("Hence,the PRODUCT IS:%d",product);
+	return 0;
+}
