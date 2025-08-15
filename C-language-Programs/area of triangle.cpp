a275a7722276917772c7523b70c3f1575af543cb
PrinceTimilsina <142810341+PrinceTimilsina@users.noreply.github.com>
2025-08-15 22:06:12 +0545

    Add files via upload

---
+++ C-language-Programs/area of triangle.cpp
@@ -0,0 +1,14 @@
+//to calcuiate the are of triangle  [area= 1/2lb]
+#include<stdio.h>
+#include<conio.h>
+int main(){
+	float l,b,area;
+	printf("Enter length");
+	scanf("%f",&l);
+	printf("Enter base");
+	scanf("%f",&b);
+    a=l*b*0.5;b 
+	printf("\n Area = %f",a);
+	gatch();
+	return 0;
+	   }
