a48b6ee8237f1c89d8d82bf38d9996bbc7a4ce38
PrinceTimilsina <142810341+PrinceTimilsina@users.noreply.github.com>
2025-08-15 22:13:23 +0545

    Add files via upload

---
+++ C-language-Programs/odd and even if else.cpp
@@ -0,0 +1,20 @@
+#include<stdio.h>
+  int main()
+  {
+  	int n;
+  	printf("Enter a number");
+  	scanf("%d",&n);
+  	if(n==0)
+  	{
+  		printf("The number is zero");
+	  }
+  	 else if(n%2==0)
+  	  {
+  	  	printf("The number is even");
+		}
+		else{
+			printf("The number is odd");
+		}
+  
+		return 0;
+  }
